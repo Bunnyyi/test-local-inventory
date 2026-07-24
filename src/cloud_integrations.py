079bf2c471803146dc0c9229e1ad22875811f87f
Annonnymmousss <jatin02012006@gmail.com>
2026-07-24 17:22:49 +0530

    test: remove Azure integration

--- src/cloud_integrations.py
+++ src/cloud_integrations.py
@@ -1,5 +1,4 @@
 import boto3
-from azure.storage.blob import BlobServiceClient
 from google.cloud import storage
 from vertexai.generative_models import GenerativeModel
 
@@ -8,7 +7,6 @@ GCP_PROJECT = "forward-dream-501807-j3"
 GCP_BUCKET = "repello-gcp-inventory-test-forward-dream-501807-j3"
 GEMINI_MODEL = "gemini-2.5-flash"
 AWS_BUCKET = "repello-inventory-example-bucket"
-AZURE_STORAGE_URL = "https://repelloinventorytest.blob.core.windows.net"
 
 
 def read_gcp_object():
@@ -25,7 +23,3 @@ def read_aws_object():
     client = boto3.client("s3", region_name="us-east-1")
     return client.get_object(Bucket=AWS_BUCKET, Key="inventory-test.json")
 
-
-def connect_azure_storage():
-    client = BlobServiceClient(account_url=AZURE_STORAGE_URL)
-    return client
